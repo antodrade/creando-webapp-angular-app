af248179b5e5fdb2895a23926dc63b52a0188fab
prueba2@gmail.com <prueba@gmail.com>
2024-08-25 20:10:53 -0500

    Flujo de Control sintaxis desde Angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [ RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,5 +13,12 @@ export class AppComponent {
   title: string = 'Hola Mundo Angular desde componente!';
 
   users: string[] = ['pepe', 'Maria', 'Juan','Andres'];
+
+  visible: boolean = false;
+
+  setVisible(): void {
+    this.visible = this.visible? false: true;
+    console.log('hemos hecho click en setVisible');
+  }
  
 }
