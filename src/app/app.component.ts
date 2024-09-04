d13d3c4cec8e8af5a17bdb16328633c9a9a58a94
prueba2@gmail.com <prueba@gmail.com>
2024-09-03 21:06:38 -0500

    Transmitiendo datos desde el componente hijo al padre con @Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet],
+  imports: [ CounterComponent,  RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string = 'Hola Mundo Angular desde componente!';
-
+  
+  subTitle = 'Contador con estado de sesión';
+  
+  
   users: string[] = ['pepe', 'Maria', 'Juan','Andres'];
-
+  
   visible: boolean = false;
-
+  
+  counter: number =0;
+  
+  ngOnInit(): void {
+   this.counter = parseInt(sessionStorage.getItem('counter')!) || 0;
+  }
   setVisible(): void {
     this.visible = this.visible? false: true;
     console.log('hemos hecho click en setVisible');
+  }
+
+  setCounter(counter: number): void{
+  this.counter = counter;
   }
  
 }
